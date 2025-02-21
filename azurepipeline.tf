287d81a7d3e4ade29b94d71f37052d080644beb0
Harshita217 <gharshita670@gmail.com>
2025-02-21 15:22:37 +0530

    Update azurepipeline.tf

--- azurepipeline.tf
+++ azurepipeline.tf
@@ -2,7 +2,7 @@ provider "azurerm" {
  subscription_id = "79c65e59-aadb-45ab-9a43-3e10d9e5225e"
     features {}
 }
-resource "azurerm_resource_group" "devops-rg-hg" {
-  name     = "harshita-devops-rg1"
+resource "azurerm_resource_group" "devops-rg-hg2" {
+  name     = "harshita-devops-rg2"
   location = "westus"
 }
